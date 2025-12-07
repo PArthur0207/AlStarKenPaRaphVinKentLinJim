68b5156950b96a3d38d5f1dbe02b140d4fcd9bd5
Winstar <james.canoy87@gmail.com>
2025-12-07 18:48:28 +0800

    feat(routes): add /dict_search route to app.py

--- dsa-team-portfolio/backend/__init__.py
+++ dsa-team-portfolio/backend/__init__.py
@@ -2,6 +2,7 @@ from .node import Node
 from .doubly_ended_queue import Queue
 from .sample_queue_implementation import CustomerQueue
 from .binary_tree_implementation import BinaryTreeManager
-
+from .binary_search_tree_implementation import DictionarySearch
 # 'from backend import *'
-__all__ = ['Node', 'Queue', 'CustomerQueue', 'BinaryTreeManager']
+__all__ = [
+    'Node', 'Queue', 'CustomerQueue', 'BinaryTreeManager', 'DictionarySearch']
